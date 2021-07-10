15b7fa93fec06922fe2688eaf77132381d4d5ca9
AyoOlu1 <72816381+AyoOlu1@users.noreply.github.com>
2021-07-10 20:22:57 +0100

    Made some changes
    Added a screenshot of the project,...

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# sidehustle-jscounter
+# Js Counter
+ ## Overview
+ This is a program that increases the count of a number and reduces it when you click on the "add count" and "lower count" button.
+
+ ## Screenshot 
+  ![](https://github.com/AyoOlu1/sidehustle/jscounter/blob/master/image/images/jscounterscreenshot.png)
+
+
+ ## Links 
+  Click [here](https://) to view the live site.
--- jsCounter.js
+++ jsCounter.js
@@ -4,23 +4,30 @@ function addCount() {
       if (i < 50) {
           i++;
       } else if (i = 50) {
-          i = 0;
-      }
+          i = 50;
+          alert("The maximum count is 50");
+      } 
+      
       document.getElementById("current-count").innerHTML = i;
-};
+      
+   };
 
-function lowerCount() {
+function lowerCount() {  
      if (i > 0) {
          i--;
-     } else if (i = 0) {
-         i = 50;
-     }
+     } else if (i == 0) {
+         i = 0;
+         alert("The minimum count is 0");
+     } 
      document.getElementById("current-count").innerHTML = i;
+   
 };
+
 
 document.getElementById("add-count").addEventListener("click", function () {
     addCount();
 });
+
 document.getElementById("lower-count").addEventListener("click", function () {
     lowerCount();
 });
